c95ad5c19492f41d68e8aaa31faa7369781f1715
Tassawer Hussain <t.tassawer@gmail.com>
2024-07-03 18:43:18 +0500

    Create the notes table column

--- database/migrations/2024_07_03_133721_create_notes_table.php
+++ database/migrations/2024_07_03_133721_create_notes_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('notes', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->string('title');
+            $table->longText('text');
             $table->timestamps();
         });
     }
